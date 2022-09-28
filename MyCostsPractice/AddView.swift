07310f90f88bd159486e73299ba0bec91d5245dd
Daniil Tyncherov <91438639+cybertatar@users.noreply.github.com>
2022-09-28 20:23:21 +0300

    Практика: контроль финансовых трат.

---
+++ MyCostsPractice/AddView.swift
@@ -0,0 +1,46 @@
+//
+//  AddView.swift
+//  MyCostsPractice
+//
+//  Created by Даниил Тынчеров on 28.09.2022.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    let types = ["Bussines", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Название", text: $name)
+                Picker("Тип", selection: $type) {
+                    ForEach(self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Стоимость",  text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Добавить")
+            .navigationBarItems(trailing: Button("Сохранить") {
+                if let actualAmount = Int(self.amount) {
+                    let item = ExpenseItem(name:  self.name, type:  self.type, amount:  actualAmount)
+                    self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+            })
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
