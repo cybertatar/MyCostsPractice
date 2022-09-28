84eb60211a3f28c671fd77ddfdac69c50807b2f0
Daniil Tyncherov <91438639+cybertatar@users.noreply.github.com>
2022-09-28 17:50:02 +0300

    Initial Commit

---
+++ MyCostsPractice/MyCostsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCostsPracticeApp.swift
+//  MyCostsPractice
+//
+//  Created by Даниил Тынчеров on 28.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyCostsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
